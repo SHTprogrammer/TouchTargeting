c8089dacaa3701e1d5f7e419bd5d2f306fce3e96
radicalappwards <radicalappwards@gmail.com>
2014-10-29 17:18:39 -0700

    move animation to onresume

--- TouchTarget/src/touch/target/MainActivity.java
+++ TouchTarget/src/touch/target/MainActivity.java
@@ -11,25 +11,77 @@ import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+/**
+ * 
+ * The starting class as declared in the Android manifest file. Handle the
+ * overall Activity calls from onCreate to onDestroy.
+ * 
+ * @author Rick
+ *
+ */
 public class MainActivity extends ActionBarActivity {
 
+	/**
+	 * Will be used to hold the pixels to be drawn on.
+	 */
 	public DrawingSurface drawingSurface;
 
+	/**
+	 * The on create method allow you start creating the view objects and
+	 * referencing them.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// get reference to our surface
 		drawingSurface = (DrawingSurface) findViewById(R.id.drawing_surface);
 		drawingSurface.setTextView((TextView) findViewById(R.id.pause_message));
+	}
 
+	/**
+	 * Declare the menu you want to use here from the menu folder.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * The method that will be used to handle the options selected from the menu
+	 * you created.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.restart:
+			drawingSurface.menuRestart();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// start the application out by modifying the view and don't accept user
+		// input until the animation ends
 		Animation anim = AnimationUtils.loadAnimation(this, R.anim.intro);
 		anim.reset();
 		anim.setAnimationListener(new AnimationListener() {
 
 			@Override
 			public void onAnimationStart(Animation animation) {
-				// TODO Auto-generated method stub
-
+				// if the animation isn't finished we shouldn't be drawing on it
+				drawingSurface.introFinished = false;
 			}
 
 			@Override
@@ -40,7 +92,8 @@ public class MainActivity extends ActionBarActivity {
 
 			@Override
 			public void onAnimationEnd(Animation animation) {
-				//this animation is longest and touch should be ready when this completes
+				// this animation is longest and touch should be ready when this
+				// completes
 				drawingSurface.introFinished = true;
 			}
 		});
@@ -53,30 +106,7 @@ public class MainActivity extends ActionBarActivity {
 
 		drawingSurface.messageTextView.clearAnimation();
 		drawingSurface.messageTextView.startAnimation(anim2);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.restart:
-			drawingSurface.menuRestart();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
+		
 		drawingSurface.onResume();
 	}
 
